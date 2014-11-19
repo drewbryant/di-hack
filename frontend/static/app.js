f913bc4ea387cedd50a06e57da2f15e2c1a4c34b
Drew Bryant <bryantd@google.com>
2014-11-19 13:48:16 -0800

    Made the number of active users within a server display/update dynamically

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,5 +1,7 @@
 var app = angular.module("game-lobby", ["firebase"]);
-app.controller("MainCtrl", function($scope, $firebase) {
+app.controller("MainCtrl", MainCtrl);
+
+function MainCtrl ($scope, $firebase, $window) {
   // Assume we've done authn and know that we're presenting user Mary now
   // TODO: wire in the firebase authn bits
   var userId = "mary-id";
@@ -8,13 +10,12 @@ app.controller("MainCtrl", function($scope, $firebase) {
   var lobbyFirebase = new Firebase("https://glaring-heat-2029.firebaseio.com/lobby");
 
   $scope.user = $firebase(userFirebase).$asObject();
-
   $scope.servers = $firebase(serverFirebase).$asObject();
 
   $scope.serverMaxUsers = 20; // minecraft constant
 
-  // $scope.addFriend = function(name) {
-  //   $scope.friends.$add({text: name});
-  // }
-  // $scope.addFriend('Alice'); $scope.addFriend('London');
-});
+}
+
+MainCtrl.prototype.getNumActiveUsers = function (server) {
+  return Object.keys(server.active).length;
+}
