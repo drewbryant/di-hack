89f3f3a12b758a40282be7cad1ba0aa9ef421777
Drew Bryant <bryantd@google.com>
2014-11-19 14:20:55 -0800

    Added scope var for syncing all users with firebase

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -4,15 +4,18 @@ app.controller("MainCtrl", MainCtrl);
 function MainCtrl ($scope, $firebase, $window) {
   // Assume we've done authn and know that we're presenting user Mary now
   // TODO: wire in the firebase authn bits
-  var userId = "mary-id";
-  var userFirebase = new Firebase("https://glaring-heat-2029.firebaseio.com/users/" + userId);
-  var serverFirebase = new Firebase("https://glaring-heat-2029.firebaseio.com/servers");
-  var lobbyFirebase = new Firebase("https://glaring-heat-2029.firebaseio.com/lobby");
-
-  $scope.user = $firebase(userFirebase).$asObject();
-  $scope.servers = $firebase(serverFirebase).$asObject();
-
+  var firebaseUrl = 'https://glaring-heat-2029.firebaseio.com';
   $scope.serverMaxUsers = 20; // minecraft constant
+
+  var userId = "mary-id";
+
+  var userRef = new Firebase(firebaseUrl + '/users/' + userId);
+  var serverRef = new Firebase(firebaseUrl + '/servers');
+  var allUsersRef = new Firebase(firebaseUrl + '/users');
+
+  $scope.user = $firebase(userRef).$asObject();
+  $scope.servers = $firebase(serverRef).$asObject();
+  $scope.allUsers = $firebase(allUsersRef).$asObject();
 
 }
 
