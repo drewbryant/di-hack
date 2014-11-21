bb7901195bb2dd1777807f899ed16844d44f17a8
Drew Bryant <bryantd@google.com>
2014-11-21 12:56:15 -0800

    Removed some debugging statements and unneeded code

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -2,23 +2,31 @@ var app = angular.module("game-lobby", ["firebase"]);
 
 app.controller("MainCtrl", MainCtrl);
 function MainCtrl ($scope, $firebase, $http) {
-  // Assume we've done authn and know that we're presenting user Mary now
   // TODO: wire in the firebase authn bits
-  var firebaseUrl = 'https://glaring-heat-2029.firebaseio.com';
+  this.firebaseUrl = 'https://glaring-heat-2029.firebaseio.com';
   $scope.serverMaxUsers = 20; // minecraft constant
 
-  var userId = "mary-id";
+  this.serversRef = new Firebase(this.firebaseUrl + '/servers');
+  this.allUsersRef = new Firebase(this.firebaseUrl + '/users');
 
-  var userRef = new Firebase(firebaseUrl + '/users/' + userId);
-  var serverRef = new Firebase(firebaseUrl + '/servers');
-  var allUsersRef = new Firebase(firebaseUrl + '/users');
-
-  $scope.user = $firebase(userRef).$asObject();
-  $scope.servers = $firebase(serverRef).$asObject();
-  $scope.allUsers = $firebase(allUsersRef).$asObject();
+  $scope.user = null;
+  $scope.servers = $firebase(this.serversRef).$asObject();
+  $scope.allUsers = $firebase(this.allUsersRef).$asObject();
 
   this.http = $http;
+  this.scope = $scope;
+  this.firebase = $firebase;
 
+  this.login('drew-id', 'the_real_ron_brn', 'Drew'); // FIXME: for debugging
+}
+
+MainCtrl.prototype.addFriend = function (minecraftUserId) {
+  console.log('addFriend()');
+  this.userRef
+    .child('friends')
+    .child(minecraftUserId)
+    .set(true);
+  console.log('addFriend() done');
 }
 
 MainCtrl.prototype.getNumActiveUsers = function (server) {
@@ -27,21 +35,130 @@ MainCtrl.prototype.getNumActiveUsers = function (server) {
   } else {
     return Object.keys(server.active).length;
   }
+};
+
+MainCtrl.prototype._getUserFirebasePath = function (userId) {
+  return this.firebaseUrl + '/users/' + userId
+}
+
+MainCtrl.prototype.login = function (userId, minecraftUserId, displayName) {
+  this.allUsersRef // FIXME: hack to auto-login during testing which clears all existing friends
+    .child(userId)
+    .set({
+      minecraftUserId: minecraftUserId,
+      online: true,
+      displayName: displayName,
+      friends: {'mary-id': true, 'alice-id': true}
+    }); // Note: clears any existing friends due to use of 'set'
+
+  // Create a sync'd object for the user, to drive the UI
+  var userRefPath = this._getUserFirebasePath(userId);
+  this.userRef = new Firebase(userRefPath);
+  this.scope.user = this.firebase(this.userRef).$asObject();
+
+  // Setup event handlers for friend adds/removes
+  var friendsRef = new Firebase(userRefPath + '/friends');
+  this.scope.friends = {};
+  // Child added event will be called once for each value in the collection
+  // (e.g., if there are already 3 friends, it will be invoked thrice immediately)
+  friendsRef.on('child_added', this.handleFriendAdded.bind(this));
+  friendsRef.on('child_removed', this.handleFriendRemoved.bind(this));
+}
+
+MainCtrl.prototype._insertFriend = function (friendUserId) {
+  // WARNING / FIXME: for the moment, assume that the arg is the real id
+  // TODO: add another lookup map of minecraftId -> userId
+  var friendRef = new Firebase(this._getUserFirebasePath(friendUserId));
+  this.scope.friends[friendUserId] = this.firebase(friendRef).$asObject();
+}
+
+MainCtrl.prototype.handleFriendAdded = function (snapshot) {
+  console.log('friend added: ', snapshot.key(), snapshot.val());
+  this._insertFriend(snapshot.key());
+}
+
+MainCtrl.prototype.handleFriendRemoved = function (snapshot) {
+  console.log('friend removed: ', snapshot.key(), snapshot.val());
+  delete this.scope.friends[snapshot.key()];
+}
+
+MainCtrl.prototype.logout = function (userId) {
+  this.userRef.child('online').set(false);
+  this.userRef = null;
+  this.user = null;
 }
 
 MainCtrl.prototype.createServer = function (serverName, diskName) {
   console.info('creating an instance...', serverName, diskName);
   var uri = '/api/create?server=' + serverName + '&disk=' + diskName;
-  console.log('create uri: ', uri);
+
+  // Create a record in Firebase that will be overwritten on successful creation of the VM
+  this.serversRef
+    .child(serverName)
+    .child('ip')
+    .child('external')
+    .set(false);
+
+  // TODO: move the content inline to avoid dealing with the modal
+  $('#create-game-server-modal').hide();
+
   this.http.get(uri)
     .success(function(data, status, headers, config) {
-      console.log('success!', arguments);
       if (status === 200) {
-        // TODO: should make a modal directive that manipulates the dismiss property of the component
-        $('#create-game-server-modal').hide();
+        console.log('success!', arguments);
+      } else {
+        console.log('success, but non-200 response: ', status);
       }
     })
     .error(function(data, status, headers, config) {
+      // TODO: add some error handling eventually
       console.log('error sending create instance request', arguments);
     });
-}
+};
+
+var smoothSpinnerOptions = {
+  lines: 15, // The number of lines to draw
+  length: 3, // The length of each line
+  width: 10, // The line thickness
+  radius: 0, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#ccc', // #rgb or #rrggbb or array of colors
+  speed: 1.2, // Rounds per second
+  trail: 58, // Afterglow percentage
+  className: 'spinner', // The CSS class to assign to the spinner
+  top: '50%', // Top position relative to parent
+  left: '88%' // Left position relative to parent
+};
+
+var blockSpinnerOptions = {
+  lines: 11, // The number of lines to draw
+  length: 4, // The length of each line
+  width: 8, // The line thickness
+  radius: 0, // The radius of the inner circle
+  corners: 0, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#ccc', // #rgb or #rrggbb or array of colors
+  speed: 1.5, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: true, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50%', // Top position relative to parent
+  left: '88%' // Left position relative to parent
+};
+
+app.directive('spinner', function() {
+  return {
+    restrict: 'E',
+    template: '<span class="text-muted">Creating...</span>',
+    replace: true,
+    link: function(scope, element, attrs) {
+      var spinner = new Spinner(blockSpinnerOptions).spin();
+      element.append(spinner.el);
+    }
+  };
+});
